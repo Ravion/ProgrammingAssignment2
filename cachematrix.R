e422984d5e114bd61a415e4d3bb95bc0d0215c7e
Ravion <ravishankar.nair@gmail.com>
2014-07-18 18:41:03 -0400

    Completed and Tested Code for Assignment 2
    Completed and Tested Code for Assignment 2 - Coursera R Programming
    Course

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,38 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Added by Ravion as part of Assignment 2 - Coursera R Programming Course
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+}
+
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+}
+
+## Sample Usage is given below
+## a<-makeCacheMatrix() 
+## a$set(matrix(1:4,2,2))
+## cacheSolve(a)
